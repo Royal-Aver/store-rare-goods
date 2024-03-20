6ebbea2220e049b9d6243ddbe99bacd087bba84d
Royal-Aver <royal_aver@rambler.ru>
2024-03-20 13:23:32 +0300

    add app user

--- store_rare_goods/urls.py
+++ store_rare_goods/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
+    path('users/', include('users.urls', namespace='users')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
